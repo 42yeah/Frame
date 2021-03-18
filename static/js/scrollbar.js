c7e76ab1fdfb003cc3f44ab423d7ab0a0c0cbb49
42yeah <42yeah@gmx.fr>
2021-03-18 16:01:47 +0800

    Fixed scrollbar?

--- static/js/scrollbar.js
+++ static/js/scrollbar.js
@@ -1,52 +1,67 @@
 // Calculate 
 
-function calibrateScrollbar(scrollBarViewport, sticky, footer) {
+function calibrateScrollbar(scrollBarViewport, sticky, footer, blogContainer, scrollBarBrick) {
     const stickyRect = sticky.getBoundingClientRect();
     const footerRect = footer.getBoundingClientRect();
+
+    const blogContainerHeight = blogContainer.getBoundingClientRect().height;
 
     const top = stickyRect.y + stickyRect.height;
     const viewingRange = footerRect.y - top;
     scrollBarViewport.style = `left: ${stickyRect.x + stickyRect.width - 10}px; top: ${top}px; height: ${viewingRange}px`;
+
+    if (viewingRange / blogContainerHeight <= 0.99) {
+        // What's the point of a scrollbar if the whole window could contain the blob post?
+        scrollBarViewport.classList.remove("invisible");
+    } else {
+        scrollBarViewport.classList.add("invisible");
+    }
+
+    const brickHeight = (viewingRange / blogContainerHeight) * viewingRange;
+
     return {
-        top, viewingRange
+        top, viewingRange, blogContainerHeight, brickHeight
     };
+}
+
+function updateScrollBarBrick(top, scrollBarBrick, blogContainerHeight, viewingRange, brickHeight) {
+    const brickPos = top / blogContainerHeight * viewingRange;
+    let h = brickHeight;
+    if (brickPos + brickHeight > viewingRange) {
+        h = viewingRange - brickPos;
+    }
+    scrollBarBrick.style = `height: ${h}px; top: ${brickPos}px;`;
 }
 
 window.addEventListener("load", () => {
     const scrollBarViewport = document.querySelector(".scrollbar-viewport");
     const sticky = document.querySelector(".sticky");
     const footer = document.querySelector(".footer");
-    let viewingRange, top;
+    const blogContainer = document.querySelector(".blog-container");
+    const scrollBarBrick = document.querySelector(".scrollbar-brick");
+    let viewingRange, top, blogContainerHeight, brickHeight;
 
-    const ret = calibrateScrollbar(scrollBarViewport, sticky, footer);
+    const ret = calibrateScrollbar(scrollBarViewport, sticky, footer, blogContainer, scrollBarBrick);
     viewingRange = ret.viewingRange;
     top = ret.top;
-
-    const scrollBarBrick = document.querySelector(".scrollbar-brick");
-    const blogContainerHeight = document.querySelector(".blog-container").getBoundingClientRect().height;
+    blogContainerHeight = ret.blogContainerHeight;
+    brickHeight = ret.brickHeight;
     
-    const brickHeight = (viewingRange / blogContainerHeight) * viewingRange;
-    if (viewingRange / blogContainerHeight <= 0.99) {
-        // What's the point of a scrollbar if the whole window could contain the blob post?
-        scrollBarViewport.classList.remove("invisible");
-    }
     scrollBarBrick.style = `height: ${brickHeight}px; top: 0;`;
 
     // After a successful calibration...
     window.addEventListener("resize", e => {
-        const ret = calibrateScrollbar(scrollBarViewport, sticky, footer);
+        const ret = calibrateScrollbar(scrollBarViewport, sticky, footer, blogContainer, scrollBarBrick);
         viewingRange = ret.viewingRange;
         top = ret.top;
+        blogContainerHeight = ret.blogContainerHeight;
+        brickHeight = ret.brickHeight;
+        updateScrollBarBrick(verticalViewport.scrollTop, scrollBarBrick, blogContainerHeight, viewingRange, brickHeight);
     });
 
     const verticalViewport = document.querySelector(".vertical-viewport");
     verticalViewport.addEventListener("scroll", e => {
-        const brickPos = (e.target.scrollTop) / blogContainerHeight * viewingRange;
-        let h = brickHeight;
-        if (brickPos + brickHeight > viewingRange) {
-            h = viewingRange - brickPos;
-        }
-        scrollBarBrick.style = `height: ${h}px; top: ${brickPos}px;`;
+        updateScrollBarBrick(e.target.scrollTop, scrollBarBrick, blogContainerHeight, viewingRange, brickHeight);
     });
 
     let dragging = false;
